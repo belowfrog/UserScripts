c7678d3575fe47c53fc22062507cfb5fdb0267ea
belowfrog <belowfrog@gmail.com>
2018-06-13 16:36:01 +0800

    docs: update description

--- github_copy_short_hash/user.js
+++ github_copy_short_hash/user.js
@@ -1,23 +1,29 @@
 // ==UserScript==
 // @name         copy 8-bit commit hash
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/belowfrog/UserScripts
 // @description  in commits page, add new copy button besides origin button for copy short commit hash
-// @author       Belowfrog
-// @include      /https?:\/\/(www\.)?github/
+// @author       belowfrog
 // @license      MIT
+// @icon         https://assets-cdn.github.com/pinned-octocat.svg
+// @include      /https?:\/\/(www\.)?github/
+// @homepage     https://github.com/belowfrog/UserScripts/tree/master/github_copy_short_hash
+// @homepageURL  https://github.com/belowfrog/UserScripts/tree/master/github_copy_short_hash
+// @supportURL   https://github.com/belowfrog/UserScripts/issues
+// @downloadURL  https://github.com/belowfrog/UserScripts/tree/master/github_copy_short_hash/user.js
+// @updateURL    https://github.com/belowfrog/UserScripts/tree/master/github_copy_short_hash/user.js
+// @version      0.2
 // @grant        none
 // ==/UserScript==
 
-(function () {
-  'use strict';
+;(function () {
+  'use strict'
   const copyBtns = document.querySelectorAll('button[data-clipboard-text]')
   copyBtns.forEach(node => {
     if (node.dataset.clipboardText.length) {
       const newNode = node.cloneNode(true)
-      newNode.dataset.clipboardText = node.dataset.clipboardText.substr(0, 8);
+      newNode.dataset.clipboardText = node.dataset.clipboardText.substr(0, 8)
       newNode.setAttribute('aria-label', 'copy 8-bit commit hash')
       node.parentNode.insertBefore(newNode, node)
     }
   })
-})();
+})()
